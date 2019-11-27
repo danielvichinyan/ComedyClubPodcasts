7fb26e5af442d4227b036a348b03820e7b19b33a
Daniel Vichinyan <dv00139@surrey.ac.uk>
2019-11-27 16:23:51 +0000

    Added Google Maps API key in credentials. Added javascript google map into the footer. Styled the podcast search page.

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,8 @@ Rails.application.routes.draw do
   get 'home/comedians', to: 'home#comedians'
   resources :podcasts do
     collection do
-      # Search route
       get :search
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
