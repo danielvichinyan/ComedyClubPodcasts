2e70d2a1ee389ac3a72ea336904d00f280f8b511
Daniel Vichinyan <dv00139@surrey.ac.uk>
2019-12-12 23:39:46 +0000

    Fixed tests in user model. All test run without errors and failures!

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,6 +10,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "should save valid user" do
     user = User.new
+    user.username = "testusername"
     user.email = "bob@example.com"
     user.password = "12345678"
     user.password_confirmation = "12345678"
@@ -19,6 +20,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "should not save duplicate user email" do
     user1 = User.new
+    user1.username = "testusername"
     user1.email = "dan@example.com"
     user1.password = "123456"
     user1.password_confirmation = "123456"
@@ -26,6 +28,7 @@ class UserTest < ActiveSupport::TestCase
     assert user1.valid?
 
     user2 = User.new
+    user2.username = "testusername"
     user2.email = "dan@example.com"
     user2.password = "123456"
     user2.password_confirmation = "123456"
